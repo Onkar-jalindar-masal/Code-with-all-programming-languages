7bf221084713007c02f95309d03b1a64d4e57815
Onkar-jalindar-masal <166086289+Onkar-jalindar-masal@users.noreply.github.com>
2024-04-05 23:20:54 +0530

    write a python program to create a tuple.

--- tuple.py
+++ tuple.py
@@ -1,9 +1,19 @@
+#write a python program to create a tuple:-
+
+
+#create tuple with same values:-
 tuple1= (10,20,30,40,50)
 print(tuple1)
 
+
+#create tuple with different values:-
 tuple2= ("ram",10,"shyam",20,30)
 print(tuple2)
 
 
+#Accessing tuple elements:-
+print(tuple1[2])
+
 output:- (10,20,30,40,50)
          ("ram",10,"shyam",20,30
+         30
