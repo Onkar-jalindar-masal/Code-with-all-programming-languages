78e30a116214230b4883dc77f14701ff37e65977
Onkar-jalindar-masal <166086289+Onkar-jalindar-masal@users.noreply.github.com>
2024-04-05 23:11:19 +0530

    write a python program to create a list in python.

---
+++ list.py
@@ -0,0 +1,13 @@
+#wap python program to create a list in python:-
+
+#create list 
+L1= [10,20,30,40,50]
+print(L1)
+
+#access list elements
+print(L1(2))
+
+
+output:- [10,20,30,40,50]
+         30
+
